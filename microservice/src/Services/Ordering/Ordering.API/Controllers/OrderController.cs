432a6bcf62ba940146c5b9be4d3623007b474114
khai.le <khai.le@ptnglobalcorp.com>
2025-04-10 16:09:42 +0700

    containerize ordering api, TODO: handle delete and update order api

--- microservice/src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ microservice/src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,23 +1,22 @@
-﻿
-
-using System.ComponentModel.DataAnnotations;
-using System.Net;
-using Contracts.Services;
+﻿using Contracts.Services;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Ordering.Application.Common.Models;
 using Ordering.Application.Features.V1.Orders;
+using Shared.SeedWork;
 using Shared.Services.Email;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace Ordering.API.Endpoints;
 
 [Route("api/v1/[controller]")]
 [ApiController]
-public class OrderEndpoints : ControllerBase
+public class OrderController : ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly ISmtpEmailService _smtpEmailService;
-    public OrderEndpoints(IMediator mediator, ISmtpEmailService smtpEmailService)
+    public OrderController(IMediator mediator, ISmtpEmailService smtpEmailService)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         _smtpEmailService = smtpEmailService;
@@ -26,6 +25,10 @@ public class OrderEndpoints : ControllerBase
     private static class RoutesName
     {
         public const string GetOrders = nameof(GetOrders);
+        public const string CreateOrder = nameof(CreateOrder);
+        public const string UpdateOrder = nameof(UpdateOrder);
+        public const string DeleteOrder = nameof(DeleteOrder);
+        // create -> Id created, update -> orderdto, delete -> no content,
     }
 
     [HttpGet("{userName}", Name = RoutesName.GetOrders)]
@@ -35,6 +38,26 @@ public class OrderEndpoints : ControllerBase
         var orders = new GetOrdersQuery(userName);
         var result = await _mediator.Send(orders);
         return Ok(result);
+    }
+
+    [HttpPost(Name = RoutesName.CreateOrder)]
+    [ProducesResponseType(typeof(ApiResult<long>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ApiResult<long>>> CreateOrder([FromBody] CreateOrderCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpPut(Name = RoutesName.UpdateOrder)]
+    public async Task<ActionResult> UpdateOrder()
+    {
+        return Ok();
+    }
+
+    [HttpDelete(Name = RoutesName.DeleteOrder)]
+    public async Task<ActionResult> DeleteOrder()
+    {
+        return Ok();
     }
 
     [HttpGet("test-email")]
