400fd38c291a610fb805926c650ab108d0201c90
khai.le <khai.le@ptnglobalcorp.com>
2025-04-11 09:48:38 +0700

    Implement delete - update method for ordering

---
+++ microservice/src/Services/Ordering/Ordering.Application/Features/V1/Orders/Commands/CreateOrder/CreateOrderValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.V1.Orders;
+
+public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderValidator()
+    {
+        RuleFor(x => x.UserName).
+            NotEmpty().WithMessage("UserName is required.").
+            MaximumLength(50).WithMessage("UserName must not exceed 50 characters.");
+        RuleFor(x => x.TotalPrice).
+            NotEmpty().WithMessage("TotalPrice is required.").
+            GreaterThan(0).WithMessage("TotalPrice must be greater than 0.");
+        RuleFor(x => x.EmailAddress).
+            NotEmpty().WithMessage("EmailAddress is required.").
+            MaximumLength(50).WithMessage("EmailAddress must not exceed 50 characters.").
+            EmailAddress().WithMessage("EmailAddress is not a valid email address.");
+        RuleFor(x => x.ShippingAddress).
+            NotEmpty().WithMessage("ShippingAddress is required.").
+            MaximumLength(200).WithMessage("ShippingAddress must not exceed 200 characters.");
+    }
+}
---
+++ microservice/src/Services/Ordering/Ordering.Application/Features/V1/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Common.Exceptions;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Domain.Entities;
+using Serilog;
+using Shared.SeedWork;
+
+namespace Ordering.Application.Features.V1.Orders;
+
+public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, ApiResult<bool>>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
+    private const string MethodName = "DeleteOrderCommandHandler";
+    public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger logger)
+    {
+        _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger;
+    }
+    public async Task<ApiResult<bool>> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        _logger.Information($"BEGIN [DeleteOrderCommandHandler] Removing Order...");
+        var orderEntity = await _orderRepository.GetByIdAsync(request.Id);
+        if (orderEntity == null) throw new NotFoundException(nameof(Order), request.Id);
+        await _orderRepository.DeleteOrder(orderEntity);
+        await _orderRepository.SaveChangesAsync();
+
+        _logger.Information($"END Order {orderEntity.Id} was successfully deleted.");
+
+        return new ApiResult<bool>(true);
+    }
+}
