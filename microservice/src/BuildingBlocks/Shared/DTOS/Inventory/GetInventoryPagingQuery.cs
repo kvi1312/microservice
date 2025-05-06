0fa1a2f696f17f1068b47f4f1dc7aa65a1bfce7f
kvi1312 <lenguyenkhai2611@gmail.com>
2025-05-06 16:06:03 +0700

    set up inventory repository

---
+++ microservice/src/BuildingBlocks/Shared/DTOS/Inventory/GetInventoryPagingQuery.cs
@@ -0,0 +1,11 @@
+﻿using Shared.SeedWork;
+
+namespace Shared.DTOS.Inventory;
+
+public class GetInventoryPagingQuery : PagingRequestParameter
+{
+    private string _itemNo;
+    public string? SearchTerm { get; set; }
+    public void SetItemNo(string itemNo) => _itemNo = itemNo;
+    public string ItemNo() => _itemNo;
+}
---
+++ microservice/src/Services/Inventory.API/Services/InventoryService.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Inventory.API.Entities;
+using Inventory.API.Extensions;
+using Inventory.API.Repositories.Abstraction;
+using Inventory.API.Services.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Shared.DTOS.Inventory;
+
+namespace Inventory.API.Services
+{
+    public class InventoryService : MongoDbRepository<InventoryEntry>, IInventoryService
+    {
+        private readonly IMapper _mapper;
+        public InventoryService(IMongoClient client, DatabaseSettings settings, IMapper mapper) : base(client, settings)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<InventoryEntryDto>> GetAllByItemNoAsync(string itemNo)
+        {
+            var entities = await FindAll().Find(x => x.ItemNo.Equals(itemNo)).ToListAsync();
+            var result = _mapper.Map<IEnumerable<InventoryEntryDto>>(entities);
+            return result;
+        }
+
+        public async Task<IEnumerable<InventoryEntryDto>> GetAllByItemNoPagingAsync(GetInventoryPagingQuery query)
+        {
+            var filterSearchTerm = Builders<InventoryEntry>.Filter.Empty;
+            var filterItemNo = Builders<InventoryEntry>.Filter.Eq(x => x.ItemNo, query.ItemNo());
+
+            if (!string.IsNullOrEmpty(query.SearchTerm))
+            {
+                filterSearchTerm = Builders<InventoryEntry>.Filter.Eq(x => x.DocumentNo, query.SearchTerm);
+            }
+
+            var andFilter = filterItemNo & filterSearchTerm;
+            var pageList = await Collection.Find(andFilter).Skip((query.PageIndex - 1) * query.PageSize).Limit(query.PageSize).ToListAsync();
+            var result = _mapper.Map<IEnumerable<InventoryEntryDto>>(pageList);
+
+            return result;
+        }
+
+        public async Task<InventoryEntryDto> GetById(string id)
+        {
+            FilterDefinition<InventoryEntry> filter = Builders<InventoryEntry>.Filter.Eq(x => x.Id, id);
+            var entity = await FindAll().Find(filter).ToListAsync();
+            var result = _mapper.Map<InventoryEntryDto>(entity);
+            return result;
+        }
+
+        public async Task<InventoryEntryDto> PurchaseItemAsync(string itemNo, PurchaseProductDto dto)
+        {
+            var entity = new InventoryEntry(ObjectId.GenerateNewId().ToString())
+            {
+                ItemNo = itemNo,
+                Quantity = dto.Quantity,
+                DocumentType = dto.DocumentType,
+            };
+
+            await CreateAsync(entity);
+            var result = _mapper.Map<InventoryEntryDto>(entity);
+            return result;
+        }
+    }
+}
