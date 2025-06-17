438fa851aaeee0b3d5b89e10e78621522f2f1347
kvi1312 <lenguyenkhai2611@gmail.com>
2025-06-17 18:54:37 +0700

    fix can not call api of order

---
+++ microservice/src/Saga.Orchestrator/Saga.Orchestrator/HttpRepository/InventoryHttpRepository.cs
@@ -0,0 +1,23 @@
+﻿using Shared.DTOS.Inventory;
+
+namespace Saga.Orchestrator.HttpRepository;
+
+public class InventoryHttpRepository : IInventoryHttpRepository
+{
+    private readonly HttpClient _client;
+
+    public InventoryHttpRepository(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public Task<string> CreateSalesOrder(SalesProductDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteOrderByDocumentNo(string documentNo)
+    {
+        throw new NotImplementedException();
+    }
+}
