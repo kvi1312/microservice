dc8f73785054a8e852144512763bad2d2f1fdeef
kvi1312 <lenguyenkhai2611@gmail.com>
2025-07-08 13:44:52 +0700

    set up identity server

---
+++ microservice/src/IdentityServer/Program.cs
@@ -0,0 +1,52 @@
+using System.Globalization;
+using System.Text;
+using Duende.IdentityServer.Licensing;
+using IdentityServer.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+var builder = WebApplication.CreateBuilder(args);
+
+try
+{
+    builder.Host.AddConfiguration();
+    builder.Host.ConfigureSeriLog();
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    var type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, "Unhandled Exception");
+}
+finally
+{
+    Log.Information("Shut down product API complete");
+    Log.CloseAndFlush();
+}
+
+static string Summary(LicenseUsageSummary usage)
+{
+    var sb = new StringBuilder();
+    sb.AppendLine("IdentityServer Usage Summary:");
+    sb.AppendLine(CultureInfo.InvariantCulture, $"  License: {usage.LicenseEdition}");
+    var features = usage.FeaturesUsed.Count > 0 ? string.Join(", ", usage.FeaturesUsed) : "None";
+    sb.AppendLine(CultureInfo.InvariantCulture, $"  Business and Enterprise Edition Features Used: {features}");
+    sb.AppendLine(CultureInfo.InvariantCulture, $"  {usage.ClientsUsed.Count} Client Id(s) Used");
+    sb.AppendLine(CultureInfo.InvariantCulture, $"  {usage.IssuersUsed.Count} Issuer(s) Used");
+
+    return sb.ToString();
+}
