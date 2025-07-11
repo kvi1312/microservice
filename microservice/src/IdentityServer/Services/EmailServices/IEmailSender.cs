de98f28d257c7308a2c5d37e63501c17a4d5245c
kvi1312 <lenguyenkhai2611@gmail.com>
2025-07-11 15:00:25 +0700

    set up smtp

---
+++ microservice/src/IdentityServer/Services/EmailServices/IEmailSender.cs
@@ -0,0 +1,6 @@
+﻿namespace IdentityServer.Services.EmailServices;
+
+public interface IEmailSender
+{
+    void SendEmail(string recipient, string subject, string body, bool isBodyHtml = false, string sender = null);
+}
